50fd83d5967ee93fdb6d7627838a5ffb0a3a2142
Frank Doesburg <frank.doesburg@gmail.com>
2015-06-09 21:41:54 +0200

    database works images can now be loaded dynamically

--- app/src/main/java/com/frankd/wttv/ArtistDetailActivity.java
+++ app/src/main/java/com/frankd/wttv/ArtistDetailActivity.java
@@ -2,14 +2,23 @@ package com.frankd.wttv;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.SQLException;
 import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by FrankD on 5-6-2015.
  */
 public class ArtistDetailActivity extends Activity {
+    private static final String TAG = "ArtistDetailActivity";
+    private Artist artist;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +35,55 @@ public class ArtistDetailActivity extends Activity {
         // Update the action bar title with the TypefaceSpan instance
         ActionBar actionBar = getActionBar();
         actionBar.setTitle(s);
+
+        //get textview
+        CustomFontTextView artistNameTV = (CustomFontTextView) findViewById(R.id.artistNameTV);
+        CustomFontTextView descriptionTV = (CustomFontTextView) findViewById(R.id.descriptionTV);
+        CustomFontTextView timeDayTV = (CustomFontTextView) findViewById(R.id.timeDayTV);
+
+
+        //get artist
+        try{
+            Intent intent = getIntent();
+            int ID = intent.getIntExtra("ID",-1);
+            this.artist = getArtistsFromDB(ID);
+
+            artistNameTV.setText(artist.getName());
+            timeDayTV.setText(artist.getDay() + " " + artist.getTime() + " @" + artist.getStage());
+            descriptionTV.setText(artist.getDescription());
+
+        }catch (Exception E){
+            Log.v(TAG, "could not find artist with in database");
+        }
+
     }
 
+    public Artist getArtistsFromDB(int ID){
+        DataBaseHelper myDbHelper = new DataBaseHelper(this);
+        myDbHelper = new DataBaseHelper(this);
+
+        try {
+
+            myDbHelper.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+
+        try {
+
+            myDbHelper.openDataBase();
+
+        }catch(SQLException sqle){
+
+            throw sqle;
+
+        }
+
+        myDbHelper.close();
+        return myDbHelper.getArtistFromDB(ID);
+    }
 
 }
--- app/src/main/java/com/frankd/wttv/TimetableActivity.java
+++ app/src/main/java/com/frankd/wttv/TimetableActivity.java
@@ -6,10 +6,26 @@ import android.os.Bundle;
 import android.text.Spannable;
 import android.text.SpannableString;
 
+import android.support.v4.view.GestureDetectorCompat;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+
 /**
  * Created by FrankD on 5-6-2015.
  */
 public class TimetableActivity extends Activity {
+    private static final String TAG = "TimetableActivity";
+
+    private float mx, my;
+    private float curX, curY;
+
+    private ScrollView vScroll;
+    private HorizontalScrollView hScroll;
+    private GestureDetectorCompat mDetector;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +43,67 @@ public class TimetableActivity extends Activity {
         ActionBar actionBar = getActionBar();
         actionBar.setTitle(s);
 
+        //set scrollviews
+        vScroll = (ScrollView) findViewById(R.id.vScroll);
+        hScroll = (HorizontalScrollView) findViewById(R.id.hScroll);
+        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+
+        float curX, curY;
+
+        switch (event.getAction()) {
+
+            case MotionEvent.ACTION_DOWN:
+                mx = event.getX();
+                my = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                curX = event.getX();
+                curY = event.getY();
+                vScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                hScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                mx = curX;
+                my = curY;
+                break;
+            case MotionEvent.ACTION_UP:
+                curX = event.getX();
+                curY = event.getY();
+                vScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                hScroll.scrollBy((int) (mx - curX), (int) (my - curY));
+                break;
+        }
+
+        return true;
+    }
+
+    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+        private static final String DEBUG_TAG = "Gestures";
+
+        @Override
+        public boolean onDown(MotionEvent event) {
+            Log.d(DEBUG_TAG,"onDown: " + event.toString());
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2,
+                               float velocityX, float velocityY) {
+            Log.v(TAG, "onFling: " + event1.toString()+event2.toString());
+            //final float distanceTimeFactor = 0.4f;
+            //final float totalDx = (distanceTimeFactor * velocityX/2);
+            //final float totalDy = (distanceTimeFactor * velocityY/2);
+            curX = event2.getX();
+            curY = event2.getY();
+            vScroll.fling((int)-velocityY);
+            hScroll.fling((int)-velocityX);
+            return true;
+        }
+    }
+
 
 }
