482fd84af5f90ab204bb3d7f746292dab1bf7f2d
Frank Doesburg <frank.doesburg@gmail.com>
2015-07-10 10:56:13 +0200

    Appothekers link in navigational drawer

--- app/src/main/java/com/frankd/wttv/MainActivity.java
+++ app/src/main/java/com/frankd/wttv/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.database.SQLException;
 import android.net.Uri;
+import android.net.http.AndroidHttpClient;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v4.widget.SwipeRefreshLayout;
